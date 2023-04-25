f76b84f8dba8169997092efeee359d060f53e27c
Varga Vivien <viviencarmenvarga@gmail.com>
2023-04-25 19:46:33 +0200

    Test

---
+++ losulya/src/TestMethods.java
@@ -0,0 +1,34 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import controlls.MainController;
+
+public class TestMethods {
+    ArrayList<String> methodNames;
+    
+    @BeforeEach
+    public void initList (){
+        this.methodNames = new ArrayList<>();
+        Method[]methods = MainController.class.getMethods();
+        //for each
+        for(Method method: methods){
+            this.methodNames.add(
+
+        
+            method.getName()
+          );
+        }
+    }
+
+@Test
+public void testCalcWeightExist(){
+    boolean actual = this.methodNames.contains("calcWeight");
+    assertTrue(actual);
+}
+}
---
+++ losulya/src/controlls/MainController.java
@@ -0,0 +1,47 @@
+package controlls;
+
+import views.MainFrame;
+
+public class MainController {
+    MainFrame mainFrame;
+
+    public MainController(){
+        this.mainFrame = new MainFrame();
+        handleEvents();
+        
+    }
+
+    private void handleEvents(){
+        this.mainFrame.getCalcButton().addActionListener(e -> {
+           
+            this.startCalc();
+
+        });
+
+    }
+    private void startCalc(){ 
+        System.out.println("muködik");
+        double perimeter = Double.parseDouble(
+            this.mainFrame.getPerimeterPanel().getValue()
+            
+        );
+
+        double length = Double.parseDouble(
+            this.mainFrame.getLongPanel().getValue()
+        );
+
+    
+     Double weight = calcWeight(perimeter, length);
+     this.mainFrame.getWeightPanel().setValue(weight.toString());
+     
+
+    }
+    public double calcWeight(double perimeter, double length){
+        
+        double weight = (perimeter * perimeter * length)/11877;
+         return weight;
+
+    }
+
+    
+}
